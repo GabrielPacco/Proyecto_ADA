c163ef33ccacb2367cccce9ffbc3111aecc7a9fc
Gleddynuri-Marbel <86405228+Gleddynuri-Marbel@users.noreply.github.com>
2022-07-10 11:47:37 -0500

    Update source.cpp

--- software/source.cpp
+++ software/source.cpp
@@ -9,5 +9,32 @@
 
 using namespace std;
 using namespace cv;
-
 //Se usa OpenCV para diferentes funciones de procesamiento de señales y actúa como GUI
+
+
+Mat src;
+Mat ReadImage(string imgName) {
+	Mat img = imread(imgName, IMREAD_COLOR);
+	if (img.empty()) {
+		cout << "No se puede leer la imagen: " << imgName << std::endl;
+		system("EXIT");
+	}
+	return img;
+}
+// Se lee la imagen mediante imgName y sino no se puede leer
+
+
+// Funcion main
+int main() {
+	String imgName;
+	cout << "Introduzca el nombre del archivo de imagen (ex. barcode.jpg): " << endl;
+	cin >> imgName;
+	// Se lee la imagen
+	src = ReadImage(imgName);
+	// Se muestra la imagen original
+	namedWindow("Original Image", WINDOW_NORMAL);
+	imshow("Original Image", src);
+	waitKey(0);
+	destroyWindow("Original Image");
+
+
