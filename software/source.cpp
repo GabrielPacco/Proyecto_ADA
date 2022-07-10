a02bfa17c25fe153f90e777d54e28845dc5c19dc
Gleddynuri-Marbel <86405228+Gleddynuri-Marbel@users.noreply.github.com>
2022-07-10 02:32:52 -0500

    Create source.cpp

---
+++ software/source.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+using namespace cv;
