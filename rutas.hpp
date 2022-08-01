19d57b70a44c5a7fcadf5f4f6b5d846d9bc2c380
unknown <etorresq@unsa.edu.pe>
2022-07-31 22:13:46 -0500

    Anadimos header para las rutas de vuelo

---
+++ rutas.hpp
@@ -0,0 +1,14 @@
+//Las rutas para los vuelos
+
+#pragma once
+#include <iostream>
+#include <list>
+#include <queue>
+#include <string>
+using namespace std;
+
+struct Nodo{
+    int inicio;
+    int fin;
+    int peso;
+};
