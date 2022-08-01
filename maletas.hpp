b020bce3962f8c3d2f2ded9d75ec18cbc4584d9d
MarkoMarcelo <83055437+MarkoMarcelo@users.noreply.github.com>
2022-08-01 12:27:43 -0500

    Update maletas.hpp

--- maletas.hpp
+++ maletas.hpp
@@ -4,5 +4,11 @@
 
 using namespace std;
 struct maleta{
-
+  int cantidad;
+  vector<float> peso;
 };
+int MenuMaletas();
+void mostrarPila (stack<maleta>);
+void agregaMaleta();
+int opcion;
+stack <maleta> maletaPila;
