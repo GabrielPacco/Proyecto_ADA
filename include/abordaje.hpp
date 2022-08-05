7ff35aeb23ace945a5fd749612779ceca71f5f11
Miguel-Deza <dmikec101@gmail.com>
2022-08-05 18:24:54 -0500

    Renombra el archivo correcto de maletas.h a maletas.hpp

--- include/abordaje.hpp
+++ include/abordaje.hpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 using namespace std;
-//Se crea la estructura del cliente para el abordaje
-struct Cliente{
+// Se crea la estructura del cliente para el abordaje
+struct Cliente
+{
     string nombre;
     int edad;
     char discapacidad;
     string destino;
-
 };
